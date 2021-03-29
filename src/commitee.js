2f526ac4a223a3865bddd93c35d499f14beecb33
Jafar Aziz <jhaziz747@gmail.com>
2021-03-29 09:38:53 +0700

    add new menu

---
+++ src/commitee.js
@@ -0,0 +1,60 @@
+const commiteeData = {
+  commitee: [
+    {
+      name: "Dr. Erni Tri Astuti, M. Math",
+      position : "Director"
+    },
+    {
+      name: "Dr. Hardius Usman, S.Si., M.Si",
+      position : "Advisor"
+    },
+    {
+      name: "Titik Harsanti, M.Si",
+      position : "Advisor"
+    },
+    {
+      name: "Ir. Agus Purwoto, M.Si",
+      position : "Advisor"
+    },
+    {
+      name: "Dr. Nasrudin, S.Si., ME",
+      position : "Person in Charge"
+    },
+    {
+      name: "Dr. Eng. Arie Wahyu Wijayanto, SST., M.T",
+      position : "Chairman"
+    },
+    {
+      name: "Dr. Rindang Bangun Prasetyo, SST., M.Si",
+      position : "Administrative Coordinator"
+    },
+    {
+      name: "Aisyah Fitri Yuniasih, SST., SE., M.Si",
+      position : "Secretariat"
+    },
+    {
+      name: "Luci Wulansari, S.Si, M.SE",
+      position : "Treasurer"
+    },
+    {
+      name: "Ika Yuni Wulansari, S.ST., M.Stat",
+      position : "Event Section"
+    },
+    {
+      name: "Ibnu Santoso, SST., M.T",
+      position : "IT and Communication Section"
+    },
+    {
+      name: "Nori Wilantika, SST., M.T.I",
+      position : "Proceedings and Publications Section"
+    },
+    {
+      name: "Lutfi Rahmatuti Maghfiroh, SST., M.T",
+      position : "Manuscript Management Section"
+    },
+    {
+      name: "Siskarossa Ika Oktora, SST., M.Stat",
+      position : "Public Relations and Documentation Section"
+    },
+  ]
+}
