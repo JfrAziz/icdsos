7c41da5b32bce1e9a35d51e4c4f85821eef71dbb
Jafar Aziz <jhaziz747@gmail.com>
2021-04-16 06:04:50 +0700

    update commitees

--- src/js/commitee.js
+++ src/js/commitee.js
@@ -1,60 +1,147 @@
 const commiteeData = {
-  commitee: [
+  commitees : [
     {
-      name: "Dr. Erni Tri Astuti, M. Math",
-      position : "Director"
+      commitee: "Steering Commitee",
+      member: [
+        {
+          name: "Dr. Erni Tri Astuti, M. Math",
+          description : "Director"
+        },
+        {
+          name: "Dr. Hardius Usman, S.Si., M.Si",
+          description : "Advisor"
+        },
+        {
+          name: "Titik Harsanti, M.Si",
+          description : "Advisor"
+        },
+        {
+          name: "Ir. Agus Purwoto, M.Si",
+          description : "Advisor"
+        },
+        {
+          name: "Dr. Nasrudin, S.Si., ME",
+          description : "Person in Charge"
+        },
+      ]
     },
     {
-      name: "Dr. Hardius Usman, S.Si., M.Si",
-      position : "Advisor"
+      commitee: "Organizing Committee",
+      member: [
+        {
+          name: "Dr. Eng. Arie Wahyu Wijayanto, SST., M.T",
+          description : "Chairman"
+        },
+        {
+          name: "Dr. Rindang Bangun Prasetyo, SST., M.Si",
+          description : "Administrative Coordinator"
+        },
+        {
+          name: "Aisyah Fitri Yuniasih, SST., SE., M.Si",
+          description : "Secretariat"
+        },
+        {
+          name: "Luci Wulansari, S.Si, M.SE",
+          description : "Treasurer"
+        },
+        {
+          name: "Ika Yuni Wulansari, S.ST., M.Stat",
+          description : "Event Section"
+        },
+        {
+          name: "Ibnu Santoso, SST., M.T",
+          description : "IT and Communication Section"
+        },
+        {
+          name: "Nori Wilantika, SST., M.T.I",
+          description : "Proceedings and Publications Section"
+        },
+        {
+          name: "Lutfi Rahmatuti Maghfiroh, SST., M.T",
+          description : "Manuscript Management Section"
+        },
+        {
+          name: "Siskarossa Ika Oktora, SST., M.Stat",
+          description : "Public Relations and Documentation Section"
+        },
+      ]
     },
     {
-      name: "Titik Harsanti, M.Si",
-      position : "Advisor"
-    },
-    {
-      name: "Ir. Agus Purwoto, M.Si",
-      position : "Advisor"
-    },
-    {
-      name: "Dr. Nasrudin, S.Si., ME",
-      position : "Person in Charge"
-    },
-    {
-      name: "Dr. Eng. Arie Wahyu Wijayanto, SST., M.T",
-      position : "Chairman"
-    },
-    {
-      name: "Dr. Rindang Bangun Prasetyo, SST., M.Si",
-      position : "Administrative Coordinator"
-    },
-    {
-      name: "Aisyah Fitri Yuniasih, SST., SE., M.Si",
-      position : "Secretariat"
-    },
-    {
-      name: "Luci Wulansari, S.Si, M.SE",
-      position : "Treasurer"
-    },
-    {
-      name: "Ika Yuni Wulansari, S.ST., M.Stat",
-      position : "Event Section"
-    },
-    {
-      name: "Ibnu Santoso, SST., M.T",
-      position : "IT and Communication Section"
-    },
-    {
-      name: "Nori Wilantika, SST., M.T.I",
-      position : "Proceedings and Publications Section"
-    },
-    {
-      name: "Lutfi Rahmatuti Maghfiroh, SST., M.T",
-      position : "Manuscript Management Section"
-    },
-    {
-      name: "Siskarossa Ika Oktora, SST., M.Stat",
-      position : "Public Relations and Documentation Section"
-    },
+      commitee: "Scientific Committee",
+      member: [
+        {
+          name: "Prof. Nicholas T. Longford",
+          description : "Imperial College London, UK"
+        },
+        {
+          name: "Dr. Fadhilla Zennifa",
+          description : "Kyushu University, Japan"
+        },
+        {
+          name: "Dr. Yoshimichi Watanabe",
+          description : "University of Yamanashi, Japan"
+        },
+        {
+          name: "Assoc. Prof. Le Hoang Son",
+          description : "Vietnam National University"
+        },
+        {
+          name: "Prof. Wisnu Jatmiko",
+          description : "Universitas Indonesia, Indonesia"
+        },
+        {
+          name: "Prof. Bambang Riyanto Trilaksono",
+          description : "Institut Teknologi Bandung, Indonesia"
+        },
+        {
+          name: "Prof. Abuzar Asra",
+          description : " Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Setia Pramana, Ph.D",
+          description : "Statistics Indonesia (BPS), Indonesia"
+        },
+        {
+          name: "Dr. Ayu Purwarianti",
+          description : "Institut Teknologi Bandung, Indonesia"
+        },
+        {
+          name: "Dr. Achmad Syahrul Choir",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Dr. Siti Muchlisoh",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Dr. Mohammad Dokhi",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Dr. Ernawati Pasaribu",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Dr. Cucu Sumarni",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Dr. Nasrudin",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Novia Budi Parwanto, Ph.D",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Dr. Timbang Sirait",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+        {
+          name: "Dr. Budiasih",
+          description : "Politeknik Statistika STIS, Indonesia"
+        },
+      ]
+    }
   ]
 }
