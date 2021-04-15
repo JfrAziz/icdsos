bed072d2ef4eddec5b7980d61aa6fede4671ad85
Jafar Aziz <jhaziz747@gmail.com>
2021-04-16 06:25:22 +0700

    update speaker & favicon

---
+++ src/js/speaker.js
@@ -0,0 +1,34 @@
+const speakerData = {
+  speaker: [
+    {
+      name: "Prof. Tsuyoshi Murata",
+      type: "Invited Speaker:",
+      description: "Tokyo Institute of Technology, Japan",
+    },
+    {
+      name: "United Nations Statistical Institute for Asia and the Pacific (UNSIAP)",
+      type: "Invited Speaker",
+      description: "Representative *"
+    },
+    {
+      name: "Prof. Budi Nurani Ruchjana",
+      type: "Invited Speaker:",
+      description: "Universitas Padjajaran, Indonesia",
+    },
+    {
+      name: "Prof. Jean-Bernard Chatelain",
+      type: "Invited Speaker:",
+      description: "University Paris 1 Pantheon Sorbonne, France",
+    },
+    {
+      name: "Dr. Alexander Kowarik",
+      type: "Invited Speaker:",
+      description: "Statistics Austria",
+    },
+    {
+      name: "Assoc. Prof. Woojoo Lee, Ph.D",
+      type: "Invited Speaker:",
+      description: "Seoul National University, South Korea",
+    },
+  ]
+}
