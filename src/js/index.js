cf64479a32e4b774b3e932dd0aaa389cf7e29393
Jafar Aziz <jhaziz747@gmail.com>
2021-04-19 01:48:40 +0700

    update index page

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+const topicsData = {
+  topics: [
+    {
+      topic: "Applied Statistics",
+      subtopics: [
+        { subtopic: "Applied Multivariate Analysis" },
+        { subtopic: "Applied Time Series Analysis" },
+        { subtopic: "Applied Spatial Statistics" },
+        { subtopic: "Applied Bayesian Statistics" },
+        { subtopic: "Microeconomics Modelling and Applications" },
+        { subtopic: "Macroeconomics Modelling and Applications" },
+        { subtopic: "Spatial Statistics" },
+        { subtopic: "Econometrics Modelling and Applications" },
+        { subtopic: "Quantitative Public Policy and Statistical Analysis" },
+        { subtopic: "Applied Statistics on Demography" },
+        { subtopic: "Applied Statistics on Population Studies" },
+        { subtopic: "Other applied statistics relevant topic" },
+      ],
+    },
+    {
+      topic: "Official Statistics",
+      subtopics: [
+        { subtopic: "Official Statistics" },
+        { subtopic: "Survey Methodology Developments" },
+        { subtopic: "Data Collection improvements" },
+        { subtopic: "Sustainable Development Goals (SDGs) Indicators Estimation" },
+        { subtopic: "Small Area Estimation (SAE)" },
+        { subtopic: "Non Response and Imputation Methods" },
+        { subtopic: "Sampling Error and Non Sampling Error Evaluation" },
+        { subtopic: "Benchmarking Regional Official Statistics" },
+        { subtopic: "Other official statistics relevant topic" }
+      ],
+    },
+    {
+      topic: "Data Science",
+      subtopics: [
+        { subtopic: "Machine Learning and Deep Learning" },
+        { subtopic: "Data Science and Artificial Intelligence (AI)" },
+        { subtopic: "Data Mining and Big Data" },
+        { subtopic: "Statistical Software" },
+        { subtopic: "Information System Development for Official Statistics" },
+        { subtopic: "Remote Sensing to Strengthen Official Statistics" },
+        { subtopic: "Other data science relevant topic" },
+      ],
+    },
+  ],
+};
