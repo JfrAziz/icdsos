dd89fa1ec7eacd15594fed2e319a79d738b723ed
Jafar Aziz <jhaziz747@gmail.com>
2021-06-14 09:04:33 +0700

    add speaker

--- src/js/speaker.js
+++ src/js/speaker.js
@@ -2,61 +2,73 @@ const speakerData = {
   isModelOpen: false,
   speakers: [
     {
-      type: "day 1",
+      type: "pre conference",
+      name: [
+        {
+          name: "Prof. Pedro Pita Barros",
+          type: "Invited Speaker",
+          description: "Universidade Nova de Lisboa, Portugal",
+          photo: "src/img/speaker/prof-pedro.jpeg",
+          profile: `Pedro Pita Barros is Professor of Economics at Universidade Nova de Lisboa, Portugal where he teaches industrial organization and health economics. He is also a research fellow at the Centre for Economic Policy Research (London). dro Pita Barros’ research focuses on issues on health economics and on regulation and competition policy. His work has covered different topics including: health expenditure determinants, waiting lists, bargaining in health care, competition policy in Portugal and in the European Union, among others. His research has appeared in many academic journals (such as the Journal of Health Economics, Health Economics, Economic Journal, European Economic Review, Journal of Industrial Economics, Journal of Economics and Management Strategy, International Journal of Health Care Finance and Economics, Health Care Management Science, among others). Pedro Pita Barros has also contributed to several books, and has published several books on health economics (written in Portuguese). Recent ones include the Handbook of Health Economics Vol II (co-editor with Mark Pauly and Tom McGuire) and Health Economics – an IO Perspective (co-authored with Xavier Martinez-Giralt). dro Pita Barros is currently Editor of the International Journal of Health Care Finance and Economics and Associate Editor of Journal of Health Economics, Health Economics, and Health Care Management Science.  He has served as Member of the Board of the Portuguese Energy Regulator (2005/2006) and on the Governmental Commission for the Financial Sustainability of the National Health Service (2006/2007). Over time he has acted as consultant for both private and public entities, in Portugal and at the European level, in the areas of health economics, competition policy and economic regulation.`,
+        },
+      ],
+    },
+    {
+      type: "Main Conference Day 1",
       name: [
         {
           name: "The Chief Statistician of Statistics Indonesia (BPS)",
           type: "Key Note Speaker:",
           description: "Statistics Indonesia (BPS), Indonesia",
           photo: "src/img/speaker/bps.png",
-          profile: `Statistics Indonesia (BPS) is led by a Chief Statistician who has the task of leading BPS in accordance with the provisions of the applicable laws, national policies and general policies in accordance with the duties of BPS, technical policies for the implementation of BPS duties which are under its responsibility, as well as fostering and carrying out cooperation with other organizations.`
+          profile: `Statistics Indonesia (BPS) is led by a Chief Statistician who has the task of leading BPS in accordance with the provisions of the applicable laws, national policies and general policies in accordance with the duties of BPS, technical policies for the implementation of BPS duties which are under its responsibility, as well as fostering and carrying out cooperation with other organizations.`,
         },
         {
           name: "Prof. Tsuyoshi Murata",
           type: "Invited Speaker:",
           description: "Tokyo Institute of Technology, Japan",
           photo: "src/img/speaker/tsuyoshi-murata.jpeg",
-          profile: `Tsuyoshi Murata is a Professor at Tokyo Institute of Technology School of Computing whose specialization field is in intelligent informatics. He is very active in writing journal paper in which he received many achievements, which one of them is Education Award of Tokyo Tech in 2019 for the implementation of Progressive Graduate Minor in Data Science and Artificial Intelligence (2021.3.2)`
+          profile: `Tsuyoshi Murata is a Professor at Tokyo Institute of Technology School of Computing whose specialization field is in intelligent informatics. He is very active in writing journal paper in which he received many achievements, which one of them is Education Award of Tokyo Tech in 2019 for the implementation of Progressive Graduate Minor in Data Science and Artificial Intelligence (2021.3.2)`,
         },
         {
           name: "Dr. Arturo Martinez Jr",
           type: "Invited Speaker",
           description: "Asian Development Bank",
           photo: "src/img/speaker/arturo-martinez.jpeg",
-          profile: `Arturo Martinez is the Statistician at the Economic Research and Regional Cooperation Department of the Asian Development Bank (ADB) where he works on Sustainable Development Goals indicator compilation, particularly poverty statistics and big data analytics. Prior to joining ADB, he was a research fellow at the University of Queensland where he also got his doctorate in Social Statistics. Some of the projects which Art is currently managing capitalize on innovative, big data, and state of the art techniques such as artificial intelligence.           `
+          profile: `Arturo Martinez is the Statistician at the Economic Research and Regional Cooperation Department of the Asian Development Bank (ADB) where he works on Sustainable Development Goals indicator compilation, particularly poverty statistics and big data analytics. Prior to joining ADB, he was a research fellow at the University of Queensland where he also got his doctorate in Social Statistics. Some of the projects which Art is currently managing capitalize on innovative, big data, and state of the art techniques such as artificial intelligence.           `,
         },
         {
           name: "Prof. Budi Nurani Ruchjana",
           type: "Invited Speaker:",
           description: "Universitas Padjajaran, Indonesia",
           photo: "src/img/speaker/budi-nurani-ruchjana.png",
-          profile: `Prof. Budi Nurani Ruchjana is a Professor of Statistics at Department of Mathematics, Faculty of Mathematics and Natural Sciences, Universitas Padjadjaran, Indonesia. She received the Ph.D. degree in Mathematics and Natural Sciences at Institut Teknologi Bandung, Indonesia (2002). Her research interest is Spatio Temporal Modeling, Stochastics Processes, Geostatistics, and its applications. She is a Head of Consortium Spatio Temporal Data Mining and Optimization Modeling in Indonesia and also she is a Head of Indonesian Ethnoscience Studies Center Universitas Padjadjaran. She was a President of the Indonesian Mathematical Society and the Dean of Faculty of Mathematics and Natural Sciences University of Padjadjaran for period 2012-2016. For 2015-2018 she is a Member of the Commission for Developing Countries International Mathematical Union (CDC IMU).`
+          profile: `Prof. Budi Nurani Ruchjana is a Professor of Statistics at Department of Mathematics, Faculty of Mathematics and Natural Sciences, Universitas Padjadjaran, Indonesia. She received the Ph.D. degree in Mathematics and Natural Sciences at Institut Teknologi Bandung, Indonesia (2002). Her research interest is Spatio Temporal Modeling, Stochastics Processes, Geostatistics, and its applications. She is a Head of Consortium Spatio Temporal Data Mining and Optimization Modeling in Indonesia and also she is a Head of Indonesian Ethnoscience Studies Center Universitas Padjadjaran. She was a President of the Indonesian Mathematical Society and the Dean of Faculty of Mathematics and Natural Sciences University of Padjadjaran for period 2012-2016. For 2015-2018 she is a Member of the Commission for Developing Countries International Mathematical Union (CDC IMU).`,
         },
       ],
     },
     {
-      type: "day 2",
+      type: "Main Conference Day 2",
       name: [
         {
           name: "Prof. Jean-Bernard Chatelain",
           type: "Invited Speaker:",
           description: "University Paris 1 Pantheon Sorbonne, France",
           photo: "src/img/speaker/jean-bernard-chatelain .jpeg",
-          profile: `Prof. Jean-Bernard Chatelain is Professor in Economics & Finance at Universitaire de France, Université Paris 1 Panthéon-Sorbonne. He has focused his research on questions of the link between credit and finance and economic activity (business investment, growth, cycles). More fundamentally, his recent research focuses on the link between economic theories and the possibility of testing them by statistical methods with as little error as possible.`
+          profile: `Prof. Jean-Bernard Chatelain is Professor in Economics & Finance at Universitaire de France, Université Paris 1 Panthéon-Sorbonne. He has focused his research on questions of the link between credit and finance and economic activity (business investment, growth, cycles). More fundamentally, his recent research focuses on the link between economic theories and the possibility of testing them by statistical methods with as little error as possible.`,
         },
         {
           name: "Dr. Alexander Kowarik",
           type: "Invited Speaker:",
           description: "Statistics Austria",
           photo: "src/img/speaker/alexandr-kowarik.jpeg",
-          profile: `Dr. Alexander Kowarik is the Head of Methods at Statistics Austria. He received the Doctor of Technology from Vienna University of Technology in 2015. He was a methodological researcher in Statistics Austria from 2008 to 2014 and Consultant of World Bank Group for generating synthetic population data in 2017. He wrote multiple publications about computational statistics using R programming and statistical package.`
+          profile: `Dr. Alexander Kowarik is the Head of Methods at Statistics Austria. He received the Doctor of Technology from Vienna University of Technology in 2015. He was a methodological researcher in Statistics Austria from 2008 to 2014 and Consultant of World Bank Group for generating synthetic population data in 2017. He wrote multiple publications about computational statistics using R programming and statistical package.`,
         },
         {
           name: "Assoc. Prof. Woojoo Lee, Ph.D",
           type: "Invited Speaker:",
           description: "Seoul National University, South Korea",
           photo: "src/img/speaker/woojoo-lee.jpeg",
-          profile: `Woojoo Lee is an associate professor in Graduate School of Public Health, Seoul National University. He received the Ph.D. in statistics from Seoul National University in 2010 and Postdoctoral degree in Medical Epidemiology and Biostatistics from Karolinska Institutet in 2012. His research interests include complex structured data analysis, application of machine learning in epidemiology, likelihood inference, causal reference, meta analysis, and bias analysis. `
+          profile: `Woojoo Lee is an associate professor in Graduate School of Public Health, Seoul National University. He received the Ph.D. in statistics from Seoul National University in 2010 and Postdoctoral degree in Medical Epidemiology and Biostatistics from Karolinska Institutet in 2012. His research interests include complex structured data analysis, application of machine learning in epidemiology, likelihood inference, causal reference, meta analysis, and bias analysis. `,
         },
         {
           name: "Dr. Walter J. Radermacher",
@@ -64,7 +76,7 @@ const speakerData = {
           description:
             "President of FENSTATS and Former Director General of Eurostat",
           photo: "src/img/speaker/walter-j-radermacher.jpeg",
-          profile: `Dr. Walter J. Radermacher was Director General of Eurostat and Chief Statistician of the European Union from 2008 to 2016. He worked at Destatis, the German Federal Statistical Office, for 30 years, ultimately as its President and Federal Returning Officer. He was the first Chair of the UN Committee of Experts on Environmental-Economic Accounting (UNCEEA) from 2005 to 2008. Since 2017 he has been a Researcher at the Department of Statistical Sciences, Sapienza University of Rome, and the President of FENStatS, the Federation of European National Statistical Societies.`
+          profile: `Dr. Walter J. Radermacher was Director General of Eurostat and Chief Statistician of the European Union from 2008 to 2016. He worked at Destatis, the German Federal Statistical Office, for 30 years, ultimately as its President and Federal Returning Officer. He was the first Chair of the UN Committee of Experts on Environmental-Economic Accounting (UNCEEA) from 2005 to 2008. Since 2017 he has been a Researcher at the Department of Statistical Sciences, Sapienza University of Rome, and the President of FENStatS, the Federation of European National Statistical Societies.`,
         },
       ],
     },
@@ -76,14 +88,14 @@ const speakerData = {
           type: "Invited Speaker:",
           description: "Moderator Day 1",
           photo: "src/img/speaker/arie.jpeg",
-          profile: `Moderator Day 1`
+          profile: `Moderator Day 1`,
         },
         {
           name: "Nucke Widowati Kusumo P., S.Si., M.Sc., Ph.D",
           type: "Invited Speaker:",
           description: "Moderator Day 2",
           photo: "src/img/speaker/nucke.jpeg",
-          profile: `Moderator Day 2`
+          profile: `Moderator Day 2`,
         },
       ],
     },
