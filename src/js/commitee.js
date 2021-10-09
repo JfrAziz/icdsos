3c063f614720b7fb4a5f2ed330e1fb06177e6f6d
Jafar Aziz <aziz@jafaraziz.com>
2021-10-10 04:20:24 +0700

    update commitee

--- src/js/commitee.js
+++ src/js/commitee.js
@@ -1,175 +1,343 @@
 const commiteeData = {
-  commitees : [
+  commitees: [
     {
       commitee: "Steering Commitee",
       member: [
         {
           name: "Dr. Erni Tri Astuti, M. Math",
-          description : "Director"
+          description: "Director",
         },
         {
           name: "Dr. Hardius Usman, S.Si., M.Si",
-          description : "Advisor"
+          description: "Advisor",
         },
         {
           name: "Dr. Titik Harsanti, M.Si",
-          description : "Advisor"
+          description: "Advisor",
         },
         {
           name: "Ir. Agus Purwoto, M.Si",
-          description : "Advisor"
+          description: "Advisor",
         },
         {
           name: "Dr. Nasrudin, S.Si., ME",
-          description : "Technical Advisor"
+          description: "Technical Advisor",
         },
-      ]
+      ],
     },
     {
-      commitee: "Coordinators of Organizing Committee",
+      commitee: "Organizing Committee",
       member: [
         {
           name: "Dr. Eng. Arie Wahyu Wijayanto, SST., M.T",
-          description : "Chairman"
+          description: "Chairman",
         },
         {
           name: "Dr. Rindang Bangun Prasetyo, SST., M.Si",
-          description : "Administrative Coordinator"
-        },
-        {
-          name: "Aisyah Fitri Yuniasih, SST., SE., M.Si",
-          description : "Secretariat"
+          description: "Administrative Coordinator",
         },
         {
           name: "Luci Wulansari, S.Si, M.SE",
-          description : "Treasurer"
+          description: "Treasurer",
+          member: [
+            {
+              name: "Tria Merina, SST",
+            },
+            {
+              name: "Tengku Mashitah Crisanty",
+            },
+          ],
+        },
+        {
+          name: "Aisyah Fitri Yuniasih, SST., SE., M.Si",
+          description: "Secretariat",
+          member: [
+            {
+              name: "Claudya Muna Asmarani, S.Tr.Stat",
+            },
+            {
+              name: "Rihadatulaisy Salsabila Nurudin",
+            },
+            {
+              name: "Iftitah Athiyyah Rahma",
+            },
+            {
+              name: "Fatikha Maulida Izzati",
+            },
+            {
+              name: "Maria A. Hasiholan Siallagan",
+            },
+          ],
         },
         {
           name: "Ika Yuni Wulansari, S.ST., M.Stat",
-          description : "Event Section"
+          description: "Event Section",
+          member: [
+            {
+              name: "Nucke Widowati Kusumo P., S.Si., M.Sc., Ph.D",
+            },
+            {
+              name: "Lia Yuliana, S.Si, M.T",
+            },
+            {
+              name: "Yuliagnis Transver Wijaya, SST., M.Sc",
+            },
+            {
+              name: "I Wayan Gede Angga Krismawan",
+            },
+            {
+              name: "Novril Ihza Razenda",
+            },
+            {
+              name: "Hijriah Nur Istifani",
+            },
+            {
+              name: "Nabila Anindita",
+            },
+          ],
         },
         {
           name: "Ibnu Santoso, SST., M.T",
-          description : "IT and Communication Section"
+          description: "IT and Communication Section",
+          member: [
+            {
+              name: "Takdir, SST.,M.T ",
+            },
+            {
+              name: "Geri Yesa Ermawan, S.Tr.Stat",
+            },
+            {
+              name: "Rahadi Jalu Yoga Utama, S.Tr.Stat",
+            },
+            {
+              name: "Muhammad Luqman, S.Tr.Stat",
+            },
+            {
+              name: "Isfan Nur Fauzi, S.Tr.Stat",
+            },
+            {
+              name: "M. Rifky Naratama Susanto",
+            },
+            {
+              name: "Jafar Husaini Aziz",
+            },
+          ],
         },
         {
           name: "Nori Wilantika, SST., M.T.I",
-          description : "Proceedings and Publications Section"
+          description: "Proceedings and Publications Section",
+          member: [
+            {
+              name: "Febri Wicaksono, SST., M.Si.",
+            },
+            {
+              name: "Dr. Rita Yuliana, S.Si., M.SE.",
+            },
+            {
+              name: "Chory Ayu Zulfaida",
+            },
+            {
+              name: "Aldias Irvan Nugraha",
+            },
+            {
+              name: "Vania Rizki Oktaviasari ",
+            },
+            {
+              name: "Maudy Insan Tania",
+            },
+            {
+              name: "Maria Shawna Cinnamon Claire",
+            },
+            {
+              name: "Muhammad Gozali Yahya",
+            },
+          ],
         },
         {
           name: "Lutfi Rahmatuti Maghfiroh, SST., M.T",
-          description : "Manuscript Management Section"
+          description: "Manuscript Management Section",
+          member: [
+            {
+              name: "Dr. Tiodora Hadumaon Siagian, M.Pop.Hum.Res",
+            },
+            {
+              name: "Yunarso Anang Sulistiadi, Ph.D",
+            },
+            {
+              name: "Dr. Azka Ubaidillah",
+            },
+            {
+              name: "Putri Fitrah Insani",
+            },
+            {
+              name: "Aulia Salsabilla ",
+            },
+            {
+              name: "Lamtutur Sri Paulina Pasaribu",
+            },
+            {
+              name: "Muhammad Fachry Nazuli",
+            },
+            {
+              name: "Ulya Adiwena",
+            },
+            {
+              name: "Elisa Imania",
+            },
+          ],
         },
+        
         {
           name: "Siskarossa Ika Oktora, SST., M.Stat",
-          description : "Public Relations and Documentation Section"
+          description: "Public Relations and Documentation Section",
+          member: [
+            {
+              name: "Nofita Istiana, SST., M.Stat. ",
+            },
+            {
+              name: "Daffa Fadhila Hidayat",
+            },
+            {
+              name: "Luthfio Febri Trihandika",
+            },
+            {
+              name: "Annisa Nur Salsabila",
+            },
+            {
+              name: "Ira Nurhanifa",
+            },
+            {
+              name: "Ilham Alifian Firmansyah ",
+            },
+            {
+              name: "Marya Zahra Aliffio Krisfianda",
+            },
+            {
+              name: "Sheyla Berlianti",
+            },
+            {
+              name: "Gloria Stephany Haman Cengga",
+            },
+            {
+              name: "Marvela Alfarosa Pardede ",
+            },
+            {
+              name: "Billy Aufa Alfarisy ",
+            },
+            {
+              name: "Siti Armanisa",
+            },
+            {
+              name: " Gilberth Rulan David Willa ",
+            }
+          ],
         },
-      ]
+      ],
     },
     {
       commitee: "Scientific Committee",
       member: [
         {
           name: "Prof. Nicholas T. Longford",
-          description : "Imperial College London, UK"
+          description: "Imperial College London, UK",
         },
         {
           name: "Prof. Vassilis Gerogiannis",
-          description : "University of Thessaly, Greece"
+          description: "University of Thessaly, Greece",
         },
         {
           name: "Dr. Fadilla Zennifa",
-          description : "Kyushu University, Japan"
+          description: "Kyushu University, Japan",
         },
         {
           name: "Dr. Yoshimichi Watanabe",
-          description : "University of Yamanashi, Japan"
+          description: "University of Yamanashi, Japan",
         },
         {
           name: "Rezzy Eko Caraka, Ph.D.",
-          description : "Seoul National University, South Korea"
+          description: "Seoul National University, South Korea",
         },
         {
           name: "Assoc. Prof. Le Hoang Son",
-          description : "Vietnam National University"
+          description: "Vietnam National University",
         },
         {
           name: "Prof. Wisnu Jatmiko",
-          description : "Universitas Indonesia, Indonesia"
+          description: "Universitas Indonesia, Indonesia",
         },
         {
           name: "Prof. Bambang Riyanto Trilaksono",
-          description : "Institut Teknologi Bandung, Indonesia"
+          description: "Institut Teknologi Bandung, Indonesia",
         },
         {
           name: "Prof. Abuzar Asra",
-          description : " Politeknik Statistika STIS, Indonesia"
+          description: " Politeknik Statistika STIS, Indonesia",
         },
         {
           name: "Setia Pramana, Ph.D.",
-          description : "Statistics Indonesia (BPS), Indonesia"
+          description: "Statistics Indonesia (BPS), Indonesia",
         },
         {
           name: "Dr. Meindra Sabri",
-          description : "Statistics Indonesia (BPS), Indonesia"
+          description: "Statistics Indonesia (BPS), Indonesia",
         },
         {
           name: "Dr. Nusar Hajarisman",
-          description : "Universitas Islam Bandung, Indonesia"
+          description: "Universitas Islam Bandung, Indonesia",
         },
         {
           name: "Dr. Ayu Purwarianti",
-          description : "Institut Teknologi Bandung, Indonesia"
+          description: "Institut Teknologi Bandung, Indonesia",
         },
         {
           name: "Dr. Anindya Apriliyanti Pravitasari",
-          description : "Universitas Padjadjaran, Indonesia"
+          description: "Universitas Padjadjaran, Indonesia",
         },
         {
           name: "Dr. Yusep Suparman",
-          description : "Universitas Padjadjaran, Indonesia"
+          description: "Universitas Padjadjaran, Indonesia",
         },
         {
           name: "Dr. Toni Toharudin",
-          description : "Universitas Padjadjaran, Indonesia"
+          description: "Universitas Padjadjaran, Indonesia",
         },
         {
           name: "Dr. Irlandia Ginanjar",
-          description : "Universitas Padjadjaran, Indonesia"
+          description: "Universitas Padjadjaran, Indonesia",
         },
         {
           name: "Dr. Yudhie Andriyana",
-          description : "Universitas Padjadjaran, Indonesia"
+          description: "Universitas Padjadjaran, Indonesia",
         },
         {
           name: "Dr. Anang Kurnia",
-          description : "IPB University, Indonesia"
+          description: "IPB University, Indonesia",
         },
         {
           name: "Dr. Achmad Syahrul Choir",
-          description : "Politeknik Statistika STIS, Indonesia"
+          description: "Politeknik Statistika STIS, Indonesia",
         },
         {
           name: "Dr. Siti Muchlisoh",
-          description : "Politeknik Statistika STIS, Indonesia"
+          description: "Politeknik Statistika STIS, Indonesia",
         },
         {
           name: "Dr. Mohammad Dokhi",
-          description : "Politeknik Statistika STIS, Indonesia"
+          description: "Politeknik Statistika STIS, Indonesia",
         },
         {
           name: "Dr. Ernawati Pasaribu",
-          description : "Politeknik Statistika STIS, Indonesia"
+          description: "Politeknik Statistika STIS, Indonesia",
         },
         {
           name: "Dr. Cucu Sumarni",
-          description : "Politeknik Statistika STIS, Indonesia"
+          description: "Politeknik Statistika STIS, Indonesia",
         },
         {
           name: "Novia Budi Parwanto, Ph.D",
-          description : "Politeknik Statistika STIS, Indonesia"
-        }
-      ]
-    }
-  ]
-}
+          description: "Politeknik Statistika STIS, Indonesia",
+        },
+      ],
+    },
+  ],
+};
