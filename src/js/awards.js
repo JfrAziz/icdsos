c84a60465847f284390a49601d1693c439f04ecb
narahunter13 <221810473@stis.ac.id>
2021-11-15 12:39:16 +0700

    Awardee(s)

--- src/js/awards.js
+++ src/js/awards.js
@@ -1,34 +1,62 @@
 const awardsData = {
   awards: [
     {
-      type: "AWARDS",
-      date: "June 2, 2021",
-      rundown: [
+      type: "Applied Statistics",
+      list: [
         {
-          time: "14.00 - 14.05",
-          schedule: "Opening from Moderator",
+          award: "1<sup>st</sup> Best Paper",
+          title: "Mobility-COVID-19 Impact Quadrant: Quantitative Approach to Analyze Community Responses to COVID-19 Pandemic",
+          author: "Usman Bustaman",
         },
         {
-          time: "14.05 - 14.15",
-          schedule: "Greetings from Director of Politeknik Statistika STIS",
+          award: "2<sup>nd</sup> Best Paper",
+          title: "The effects of price, income, and household characteristics on ultra-processed food consumption in Jakarta, Indonesia",
+          author: "Atika Putri Syatira, Ekaria",
         },
         {
-          time: "14.15 - 15.00",
-          schedule: `Speaker’s Presentation: <br/>
-          Prof. Pedro Pita Barros (Universidade Nova de Lisboa, Portugal)<br/>
-          Topic: “Shared Decision Making in the Portuguese NHS: choosing wisely?”`,
+          award: "Best Presenter",
+          title: "Digital divide and a spatial investigation of convergence in ICT development across provinces in Indonesia",
+          author: "Herdina Ramadhanti",
+        },
+      ],
+    },
+    {
+      type: "Data Science",
+      list: [
+        {
+          award: "1<sup>st</sup> Best Paper",
+          title: "Development of application providing public’s perspectives on official statistical indicators",
+          author: "Novi Kanadia, Siti Mariyah",
         },
         {
-          time: "15.00 - 15.05",
-          schedule: "Ice Breaking: Photo Session",
+          award: "2<sup>nd</sup> Best Paper",
+          title: "Cluster Analysis of COVID-19 Impact on Poverty in Indonesia Using Self-Organizing Map Algorithm",
+          author: "Ika Nur Laily Fitriana, Mohammad Okky Mabruri",
         },
         {
-          time: "15.05 - 15.50",
-          schedule: "Q&A",
+          award: "Best Presenter",
+          title: "Classification of Village Development Index at Regency/ Municipality Level using Bayesian Network Approach with K-Means Discretization",
+          author: "Nasiya Alifah Utami, Arie Wahyu Wijayanto",
+        },
+      ],
+    },
+    {
+      type: "Official Statistics",
+      list: [
+        {
+          award: "1<sup>st</sup> Best Paper",
+          title: "Adoption of Agriculture Mechanization on Paddy Farmers in Indonesia: Demographic Determinants, Internet Access Influence, and the Impact of Adoption on the Yield",
+          author: "Kadir, Octavia Rizky Prasetyo",
         },
         {
-          time: "15.50 - 16.00",
-          schedule: "Closing from Moderator",
+          award: "2<sup>nd</sup> Best Paper",
+          title: "Female worker problems : skill mismatch versus working hours mismatch",
+          author: "Jamalludin",
+        },
+        {
+          award: "Best Presenter",
+          title: "Financial Inclusion Effect on Core Poverty During the Pandemic Period in East Java",
+          author: "Abdus Salam, Hermanto",
         },
       ],
     },
